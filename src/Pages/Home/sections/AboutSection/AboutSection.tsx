9c1f970d8f8d2d016093be5e310b576a453d4d5e
Marta-r-soares <marta.soaresadm@gmail.com>
2026-04-09 22:28:56 -0300

    Fix: projectsSection

---
+++ src/Pages/Home/sections/AboutSection/AboutSection.tsx
@@ -0,0 +1,78 @@
+import { Box, Card, Container, Grid, Typography, styled } from "@mui/material"
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import SchoolIcon from '@mui/icons-material/School';
+import AnimationComponent from "../../../../components/AnimationComponent/AnimationComponent";
+
+const AboutSection: React.FC = () => {
+
+    const StyledCard = styled(Card)(({ theme }) => ({
+        padding: "10px 10px",
+        textAlign: "center",
+        marginBottom: "10px",
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.light
+        }
+    }));
+
+    const skillsSet = [
+        "Javascript", "React", "NodeJS", "Git", "HTML", "CSS3", "AWS", "Wordpress", "Canva", "Figma"
+    ]
+
+    return (
+        <>
+            <Container maxWidth="lg">
+                <Box id="about" pt={5} mb={3}>
+                    <Typography variant="h2" textAlign="center" >Sobre</Typography>
+                </Box>
+                <Grid container spacing={2} display="flex" justifyContent="center" pb={3}>
+                    <Grid item xs={9} md={2.5}>
+                        <AnimationComponent moveDirection="right">
+                            <StyledCard variant="outlined">
+                                <WorkspacePremiumIcon />
+                                <Typography textAlign="center" fontWeight={600}>Experiência</Typography>
+                                <Typography textAlign="center">1+ ano</Typography>
+                                <Typography textAlign="center">Desenvolvedora Frontend</Typography>
+                            </StyledCard>
+                        </AnimationComponent>
+                    </Grid>
+                    <Grid item xs={9} md={2.5}>
+                        <AnimationComponent moveDirection="left">
+                            <StyledCard variant="outlined">
+                                <SchoolIcon />
+                                <Typography textAlign="center" fontWeight={600}>Educação</Typography>
+                                <Typography textAlign="center">MBA</Typography>
+                                <Typography textAlign="center">Desenvolvimento Full Stack</Typography>
+                            </StyledCard>
+                        </AnimationComponent>
+
+                    </Grid>
+                </Grid>
+                <Box pb={1}>
+                    <Typography>
+                    Desenvolvedora Front-End com experiência em desenvolvimento Full Stack.
+                    Conhecimentos em Cloud Computing (AWS - Cloud Foundations).
+                    Experiência em criação de sites responsivos, interfaces web, landing pages, aplicações web e telas para apps mobile.
+                    Background em Administração e Gestão de Projetos de Tecnologia, com foco em entregas ágeis e orientação a resultados.
+                    </Typography>
+                </Box>
+                <hr />
+                <Box id="skills" pt={1} mb={3}>
+                    <Typography variant="h3" textAlign="center" fontWeight={300}>Skills</Typography>
+                </Box>
+                <Box mb={5}>
+                    <Grid container spacing={3} justifyContent="center">
+                        {skillsSet.map((skill, index) => (
+                            <Grid item key={index} xs={5} sm={4} md={2} lg={2}>
+                                <StyledCard variant="outlined">
+                                    {skill}
+                                </StyledCard>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+            </Container>
+        </>
+    )
+}
+
+export default AboutSection
---
+++ src/theme.tsx
@@ -0,0 +1,21 @@
+
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+
+let theme = createTheme({
+    typography: {
+        fontFamily: [
+            '"Helvetica Neue"',
+        ].join(','),
+    },
+    palette: {
+        primary: {
+            main: '#04051d',
+        },
+        secondary: {
+            main: '#48f154',
+        },
+    },
+});
+theme = responsiveFontSizes(theme);
+
+export default theme;
