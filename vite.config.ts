f2dd9afbc1106078d0d7a3dee57f09b92cb942b4
Marta-r-soares <marta.soaresadm@gmail.com>
2026-04-15 13:47:43 -0300

    fix:Corrigindo conflito no vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/",
   plugins: [svgr(), react()],
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> f065dffc5a88f998095be41b36583ad7932c71ec
